077d3e088474bcc3907a0775daf0028a6891bddc
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-10-10 00:17:35 -0300

    update

--- AT1/js/script.js
+++
@@ -1,8 +0,0 @@
-
-(function () {
-    const menuToggle = document.querySelector(".menu")
-    menuToggle.onclick = function (e) {
-        const body = document.body
-        body.classList.toggle('togle')
-    }
-})()
